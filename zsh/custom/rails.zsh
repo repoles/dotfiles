0bc7948e09e19fc0d39af4f9f2be804fc514691e
Lucas Repolês <lucas@repoles.com>
2022-12-15 21:48:41 -0300

    Adiciona plugins rails e bundle baseados no oh-my-zsh

--- zsh/custom/rails.zsh
+++ zsh/custom/rails.zsh
@@ -1,3 +1,31 @@
+# https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/rails/rails.plugin.zsh
+
+# rails command wrapper
+function _rails_command () {
+  if [ -e "bin/rails" ]; then
+    bin/rails $@
+  else
+    command rails $@
+  fi
+}
+
+alias rails='_rails_command'
+compdef _rails_command=rails
+
+# rake command wrapper
+function _rake_command () {
+  if [ -e "bin/rake" ]; then
+    bin/rake $@
+  elif type bundle &> /dev/null && [[ -e "Gemfile" || -e "gems.rb" ]]; then
+    bundle exec rake $@
+  else
+    command rake $@
+  fi
+}
+
+alias rake='_rake_command'
+compdef _rake_command=rake
+
 # Log aliases
 alias devlog='tail -f log/development.log'
 alias prodlog='tail -f log/production.log'
