3680882d151591a6852c6606e1a8102fab6cb207
Lucas Repolês <lucas@repoles.com>
2025-07-28 14:31:46 -0300

    Create a fish function to display the 20 most used commands

---
+++ fish/.config/fish/functions/top_cmd.fish
@@ -0,0 +1,27 @@
+function top_cmd
+    # Get total number of commands in history
+    set total (history | wc -l)
+
+    # Prevent division by zero
+    if test $total -eq 0
+        echo "No history available."
+        return 1
+    end
+
+    # to ignore sudo in aws bellow, use {if ($1 == "sudo") print $2; else print $1}
+
+    # Process and format output
+    history \
+        | awk '{print $1}' \
+        | sort \
+        | uniq -c \
+        | sort -nr \
+        | head -n 20 \
+        | awk -v total=$total 'BEGIN { OFS="\t" }
+            {
+                count = $1;
+                cmd = $2;
+                pct = (count / total) * 100;
+                printf " %2d. %5d %6.2f%%  %s\n", NR, count, pct, cmd
+            }'
+end
