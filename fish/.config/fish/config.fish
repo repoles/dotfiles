d5fb774b4ffca4c1ed168ba248cefd46da56ab81
Lucas Repolês <lucas@repoles.com>
2025-07-30 13:04:10 -0300

    Initialize zoxide and add Scripts directory to the path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,9 +5,14 @@ end
 if status is-interactive
     set -g fish_greeting
 
+    set -g fish_color_command blue
+    # set -g fish_color_param normal
+
+    zoxide init fish --cmd j | source
+
     set -x STARSHIP_CONFIG $XDG_CONFIG_HOME/starship/starship.toml
     starship init fish | source
 
-    fish_add_path --append $HOMEBREW_PREFIX/opt/mysql@8.4/bin
+    fish_add_path $HOMEBREW_PREFIX/opt/mysql@8.4/bin $HOME/Code/Scripts
     [ "$TERM_PROGRAM" = "Apple_Terminal" ] && function fish_title; end
 end
