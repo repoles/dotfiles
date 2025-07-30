f231b7bd321135120bd4178aabb8eaf157446460
Lucas Repolês <lucas@repoles.com>
2025-07-30 13:06:48 -0300

    Export path settings for irb and aws-cli

--- fish/.config/fish/conf.d/exports.fish
+++ fish/.config/fish/conf.d/exports.fish
@@ -6,5 +6,11 @@ set -x XDG_STATE_HOME $HOME/.local/state
 set -x IRBRC $XDG_CONFIG_HOME/irb/irbrc.rb
 mkdir -p $XDG_DATA_HOME/irb # where irb history will be saved
 
+set -x PRYRC $XDG_CONFIG_HOME/pry/pryrc.rb
+
 set -x MYSQL_HISTFILE $XDG_DATA_HOME/mysql/history
 mkdir -p (dirname $MYSQL_HISTFILE)
+
+set -x AWS_CONFIG_FILE $XDG_CONFIG_HOME/aws/config
+# I'm using aws-vault now (https://github.com/99designs/aws-vault)
+# set -x AWS_SHARED_CREDENTIALS_FILE $XDG_CONFIG_HOME/aws/credentials
