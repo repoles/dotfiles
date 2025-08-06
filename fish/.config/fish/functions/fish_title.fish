45ea2a502b35782464771552951cdba41ca5ae81
Lucas Repolês <lucas@repoles.com>
2025-08-06 12:47:35 -0300

    Adjust terminal title to my preferences

--- fish/.config/fish/functions/fish_title.fish
+++ fish/.config/fish/functions/fish_title.fish
@@ -1,8 +1,8 @@
 function fish_title
   set -l command (status current-command)
   if test "$command" = fish
-    echo -- (prompt_pwd -d 1 -D 1)
+    echo -- (prompt_pwd -d 1 -D 1 | string shorten --max 20 --left)
   else
-    echo -- (string sub -l 20 -- $argv[1])
+    echo -- (string sub -l 20 -- $argv[1] | string trim)
   end
 end
