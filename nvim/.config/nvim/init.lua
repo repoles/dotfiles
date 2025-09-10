5e67c3dcecf06e3aa243c0b08d3f9c25a3bcc310
Lucas Repolês <lucas@repoles.com>
2025-09-09 23:25:12 -0300

    Stores the last cursor position in the edited file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,3 +3,14 @@ vim.opt.number = true
 
 -- Enable relative line numbers
 -- vim.opt.relativenumber = true
+
+-- Remember last cursor position
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local row, col = unpack(vim.api.nvim_buf_get_mark(0, '"'))
+    if row > 0 and row <= vim.api.nvim_buf_line_count(0) then
+      vim.api.nvim_win_set_cursor(0, {row, col})
+    end
+  end,
+})
+
