b65ce7bdbe5dac8101e4b56ab80f2a1a85a644a8
Lucas Repolês <lucas@repoles.com>
2025-09-09 23:19:44 -0300

    Remove the hostname prefix from the title during an SSH session
    This information is already being added to the starfish prompt.

--- fish/.config/fish/functions/fish_title.fish
+++ fish/.config/fish/functions/fish_title.fish
@@ -1,18 +1,9 @@
 function fish_title
-    set -l command (status current-command)
-
-    set -l ssh_prefix
-    if set -q SSH_TTY; and not set -q TMUX
-        # prefix hostname if in a SSH session but not in a TMUX session
-        set ssh "["(prompt_hostname | string sub -l 10)"]"
-    end
-
-    # if test "$command" = fish
     if test (status current-command) = fish
         # no command is running, display current directory
-        echo -- $ssh (prompt_pwd -d 1 -D 1)
+        echo -- (prompt_pwd -d 1 -D 1)
     else
         # a command is running, display the command
-        echo -- $ssh (string sub -l 20 -- $argv[1] | string trim)
+        echo -- (string sub -l 20 -- $argv[1] | string trim)
     end
 end
