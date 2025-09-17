98131a4576c3e21c6a3f7125e26c320880184d3d
Lucas Repolês <lucas@repoles.com>
2025-09-17 20:28:46 -0300

    Adjust gem and bundler to XDG

--- fish/.config/fish/conf.d/exports.fish
+++ fish/.config/fish/conf.d/exports.fish
@@ -6,8 +6,19 @@ set -x XDG_STATE_HOME $HOME/.local/state
 set -x EDITOR nvim
 set -x VISUAL zed --wait
 
+# We use mise to manage Ruby installations. However,
+# we want to keep the Ruby that comes installed on
+# macOS unchanged and without adding new gems.
+# set -x GEM_HOME $XDG_DATA_HOME/gem
+# set -x GEM_PATH $GEM_HOME
+set -x GEM_SPEC_CACHE $XDG_CACHE_HOME/gem
+
+set -x BUNDLE_USER_CONFIG $XDG_CONFIG_HOME/bundle/config.yml
+set -x BUNDLE_USER_PLUGIN $XDG_DATA_HOME/bundle
+set -x BUNDLE_USER_CACHE $XDG_CACHE_HOME/bundle
+
 set -x IRBRC $XDG_CONFIG_HOME/irb/irbrc.rb
-mkdir -p $XDG_DATA_HOME/irb # where irb history will be saved
+mkdir -p $XDG_DATA_HOME/irb # where irb history must be saved
 
 set -x PRYRC $XDG_CONFIG_HOME/pry/pryrc.rb
 
