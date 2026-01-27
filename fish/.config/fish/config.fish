67c31205aa22827501334bb0ba00300c7ea5d9d5
Lucas Repolês <lucas@repoles.com>
2026-01-27 11:42:22 -0300

    Tweak Fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,4 @@
 if status is-login
-    /opt/homebrew/bin/brew shellenv | source
 end
 
 if status is-interactive
@@ -10,7 +9,7 @@ if status is-interactive
     # https://fishshell.com/docs/current/interactive.html#syntax-highlighting-variables
     set -g fish_color_command green
     set -g fish_color_error red
-    # set -g fish_color_param normal
+    set -g fish_color_param normal
     set -g fish_color_cwd blue
     set -g fish_color_cwd_root red
     set -g fish_color_comment purple --italics
@@ -28,25 +27,16 @@ if status is-interactive
     set -g __fish_git_prompt_char_upstream_ahead ↑
     set -g __fish_git_prompt_char_upstream_behind ↓
 
+    /opt/homebrew/bin/brew shellenv | source
+
     zoxide init fish --cmd j | source
     fzf --fish | source
 
     # set -x STARSHIP_CONFIG $XDG_CONFIG_HOME/starship/starship.toml
     # starship init fish | source
 
-    fish_add_path $HOMEBREW_PREFIX/opt/mysql@8.4/bin $HOME/Code/Scripts
+    fish_add_path $HOMEBREW_PREFIX/opt/mysql@8.4/bin $HOME/.local/bin
     [ "$TERM_PROGRAM" = "Apple_Terminal" ] && function fish_title; end
 
-    set icloud ~/Library/Mobile\ Documents/com\~apple\~CloudDocs
-
-    # enable_transience # does not work well with tmux
-
-    # function starship_transient_prompt_func
-    #     starship module time
-    #     starship module character
-    # end
-
-    # function starship_transient_rprompt_func
-    #     starship module time
-    # end
+    set icloud ~/"Library/Mobile Documents/com~apple~CloudDocs"
 end
