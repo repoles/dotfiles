dcdf9dcaa2ba45fbe280d6080345f0841cec1caf
Lucas Repolês <lucas@repoles.com>
2026-04-20 09:08:49 -0300

    Increase CMD_DURATION threshold for tmux notifications from 5 s to 5 m

--- fish/.config/fish/conf.d/tmux_notify.fish
+++ fish/.config/fish/conf.d/tmux_notify.fish
@@ -3,7 +3,7 @@ if set -q TMUX
         set -l last_status $status
 
         # Threshold: 5 minutes = 300,000ms
-        test $CMD_DURATION -le 5000; and return
+        test $CMD_DURATION -le 300000; and return
 
         # Don't notify if the user is still viewing the command's window:
         # session must be attached and the window must still be the active one
@@ -22,6 +22,7 @@ if set -q TMUX
         set -l short_cmd (string shorten --max 40 -- $argv[1])
         set -l duration (humanize_duration $CMD_DURATION)
 
+        # -d 0 to show the message until dismissed
         tmux display-message -d 0 -t $TMUX_PANE "[#S] #I:#W $symbol $short_cmd ($duration)"
     end
 end
