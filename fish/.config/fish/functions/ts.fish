592dd996c1083eb8caafaf70f948c3743f65ba68
Lucas Repolês <lucas@repoles.com>
2026-05-05 10:12:10 -0300

    Displays the current window name for each fzf listing session

--- fish/.config/fish/functions/ts.fish
+++ fish/.config/fish/functions/ts.fish
@@ -1,19 +1,19 @@
 function ts --wraps="tmux list-sessions" --description "Select an open tmux session"
     set now (date +%s)
 
-    set selected (tmux list-sessions -F "#{session_activity}	#{session_name}	#{session_windows}	#{session_attached}" $argv \
+    set selected (tmux list-sessions -F "#{session_activity}	#{session_name}	#{session_windows}	#{window_name}	#{session_attached}" $argv \
         | sort --reverse --numeric-sort \
         | while read --local line
             set parts (string split \t -- $line)
 
-            if test "$parts[4]" -gt 0
-                echo "$parts[2]: $parts[3] windows (current session)"
+            if test "$parts[5]" -gt 0
+                echo "$parts[2]: $parts[3] windows [$parts[4]] (current session)"
             else
                 set last_accessed (string split ' ' (humanize_duration (math --scale=0 "($now - $parts[1]) * 1000")))[1]
-                echo "$parts[2]: $parts[3] windows (accessed $last_accessed ago)"
+                echo "$parts[2]: $parts[3] windows [$parts[4]] (accessed $last_accessed ago)"
             end
         end \
-        | fzf --tmux center,60%,50%)
+        | fzf --tmux center,70%,50%)
 
     if test -z "$selected"
         return
