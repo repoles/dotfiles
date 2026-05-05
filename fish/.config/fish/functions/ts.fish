9ac7ae5c0dd1abe9442eaaa5e9089e76006c2e33
Lucas Repolês <lucas@repoles.com>
2026-05-05 10:21:19 -0300

    Displays alerts section for each fzf listing session

--- fish/.config/fish/functions/ts.fish
+++ fish/.config/fish/functions/ts.fish
@@ -1,17 +1,25 @@
 function ts --wraps="tmux list-sessions" --description "Select an open tmux session"
     set now (date +%s)
 
-    set selected (tmux list-sessions -F "#{session_activity}	#{session_name}	#{session_windows}	#{window_name}	#{session_attached}" $argv \
+    set selected (tmux list-sessions -F "#{session_activity}	#{session_name}	#{session_windows}	#{window_name}	#{session_alerts}	#{session_attached}" $argv \
         | sort --reverse --numeric-sort \
         | while read --local line
             set parts (string split \t -- $line)
+            set session_status
+            set alerts
 
-            if test "$parts[5]" -gt 0
-                echo "$parts[2]: $parts[3] windows [$parts[4]] (current session)"
+            if test -n "$parts[5]"
+                set alerts " [$parts[5]]"
+            end
+
+            if test "$parts[6]" -gt 0
+                set --append session_status "current session"
             else
                 set last_accessed (string split ' ' (humanize_duration (math --scale=0 "($now - $parts[1]) * 1000")))[1]
-                echo "$parts[2]: $parts[3] windows [$parts[4]] (accessed $last_accessed ago)"
+                set --append session_status "accessed $last_accessed ago"
             end
+
+            echo "$parts[2]: $parts[3] windows [$parts[4]]$alerts ("(string join ', ' $session_status)")"
         end \
         | fzf --tmux center,70%,50%)
 
