50c2725f2961d1910604c4ecebb55404b7eb326c
Lucas Repolês <lucas@repoles.com>
2026-06-11 23:25:34 -0300

    Show git repo root un-abbreviated in fish prompt path

--- fish/.config/fish/functions/custom_prompt_pwd.fish
+++ fish/.config/fish/functions/custom_prompt_pwd.fish
@@ -14,26 +14,48 @@ function custom_prompt_pwd
         set path "~/"(string replace -- "$HOME/" '' $path)
     end
 
+    # Find the enclosing git repository root, if any. The repo root is kept
+    # spelled out in full so the project name stays readable.
+    set -l git_root
+    set -l dir $PWD
+    while test -n "$dir"
+        if test -e "$dir/.git"
+            set git_root $dir
+            break
+        end
+        set dir (string replace -r '/[^/]*$' '' -- $dir)
+    end
+
     set -l parts (string split / -- $path)
     set -l current $parts[-1]
     set -l prefix_parts
 
-    # Abbreviate parent directories to first letter
+    # Abbreviate parent directories to their first letter, except the git
+    # repository root, which is shown in full.
     if test (count $parts) -gt 1
+        set -l abs ''
         for part in $parts[1..-2]
-            if test -z "$part"
-                set -a prefix_parts ''
-                continue
-            end
             if test "$part" = '~'
+                set abs $HOME
                 set -a prefix_parts '~'
                 continue
             end
-            set -a prefix_parts (string replace -r '^(\.?[^/]).*$' '$1' -- $part)
+            if test -z "$part"
+                set abs ''
+                set -a prefix_parts ''
+                continue
+            end
+            set abs "$abs/$part"
+
+            if test -n "$git_root"; and test "$abs" = "$git_root"
+                set -a prefix_parts $part
+            else
+                set -a prefix_parts (string replace -r '^(\.?[^/]).*$' '$1' -- $part)
+            end
         end
     end
 
-    # Build and print the full path
+    # Build and print the full path (the current directory is never abbreviated)
     set -a prefix_parts "$current"
     set -l result (string join / -- $prefix_parts)
     printf '%s%s%s' "$pwd_color" "$result" "$color_reset"
